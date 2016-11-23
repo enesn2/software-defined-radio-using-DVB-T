d596a79b4239cfe4f2a1d81f0c780c7d14c59c8d
enesk <ek2379@nyu.edu>
2016-11-23 00:25:35 -0500

    FM radio no plots

---
+++ fm-radio-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+apt-get update  # update list of available software  
+apt-get -y install git cmake libusb-1.0-0-dev python python-pip python-dev
+apt-get -y install python-scipy python-numpy python-matplotlib
+
+
+# Remove other RTL-SDR driver, if it is loaded
+modprobe -r dvb_usb_rtl28xxu
+
+git clone https://github.com/steve-m/librtlsdr  
+cd librtlsdr  
+mkdir build  
+cd build  
+cmake ../  
+make  
+make install  
+ldconfig  
+
+cd
+
+pip install pyrtlsdr   
+
+wget https://raw.githubusercontent.com/keenerd/rtl-sdr-misc/master/heatmap/flatten.py
+
+
